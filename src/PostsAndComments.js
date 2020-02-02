af5d48c0824d8846a2af58149198c5065808ca1a
Ivan Prokofyev <ivan.prokofyev@gmail.com>
2020-02-02 03:42:12 +0300

    Рустам добился своего

--- src/PostsAndComments.js
+++ src/PostsAndComments.js
@@ -7,12 +7,21 @@ class App extends Component {
     //     super(props)
         state = {
         // _meta: {},
-        posts: []
+        posts: [],
+        comments: []
         }
     // }
     
     componentDidMount() {
         const url = "https://jsonplaceholder.typicode.com/posts"
+        const comments = "https://jsonplaceholder.typicode.com/comments"
+
+        fetch(comments)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ comments: data })
+            // console.log(data.result);
+            })
 
         fetch(url)
             .then(response => response.json())
@@ -23,16 +32,30 @@ class App extends Component {
     }
 
     render() {
-        const {posts} = this.state;
+        const {posts} = this.state
+        const {comments} = this.state
+          const resultArray = posts.map(post => {
+           const filteredComments = comments.filter(comment => comment.postId == post.id)
+           const commentsResult = filteredComments.map(comment => <p>{comment.body}</p>)
+
+           return (
+            <div>
+            <p>{post.body}</p>
+            <p>{commentsResult}</p>
+            </div>
+            )
+
+        });
+          return <div>{resultArray}</div>
         // console.log(posts)
 
-        const all_post = posts.map(post => {
-            // console.log(entry);
-            // return <li key={user.id}>{user.first_name}</li>
-            return <p key={post.id}>{post.body}</p>
-        })
+        // const all_post = posts.map(post => {
+        //     // console.log(entry);
+        //     // return <li key={user.id}>{user.first_name}</li>
+        //     return <p key={post.id}>{post.body}</p>
+        // }
 
-        return <div className="container">{all_post}</div>
+        // return <div className="container">{all_post}</div>
         // return <div className="container">pfff...</div>
     }
 }
